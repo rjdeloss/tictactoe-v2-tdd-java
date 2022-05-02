50f200923a735652521dabccc3f22760585b4ae9
Rafiluis De Los Santos <rjdeloss@gmail.com>
2022-05-02 17:13:38 -0400

    fix directory for testing and add failing test

---
+++ app/src/test/java/tttTDD/Java/PlayerTest.java
@@ -0,0 +1,12 @@
+package tttTDD.Java;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PlayerTest{
+
+    @Test
+    public void passingTest() {
+        Assert.assertEquals(3, 6);
+    }
+}
