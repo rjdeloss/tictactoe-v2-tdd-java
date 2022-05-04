9fe1b30a9a58c0ee37a43554fef387c3c441bd7b
Rafiluis De Los Santos <rjdeloss@gmail.com>
2022-05-04 15:24:23 -0400

    write tests for Board class TDD style

--- app/src/main/java/tttTDD/Java/Board.java
+++ app/src/main/java/tttTDD/Java/Board.java
@@ -1,4 +1,21 @@
 package tttTDD.Java;
 
 public class Board {
+    private String[] board;
+
+    public Board() {
+        this.board = new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+    }
+
+    public String[] getBoard() {
+        return this.board;
+    }
+
+    public String getSpace(int location) {
+        return this.board[location];
+    }
+
+    public void updateBoard(int location, String marker) {
+        this.board[location] = marker;
+    }
 }
--- app/src/test/java/tttTDD/Java/BoardTest.java
+++ app/src/test/java/tttTDD/Java/BoardTest.java
@@ -8,9 +8,28 @@ public class BoardTest {
     Board board;
 
     @Test
-    public void getBoardInstance() {
+    public void getBoardInstanceWithCorrectLength() {
         board = new Board();
+
         Assert.assertEquals(board.getBoard().length, 9);
+    }
+
+    @Test
+    public void getBoardSpace() {
+        board = new Board();
+
+        Assert.assertEquals(board.getSpace(5), "6");
+        Assert.assertNotEquals(board.getSpace(5), "5");
+        Assert.assertEquals(board.getSpace(3), "4");
+        Assert.assertEquals(board.getSpace(8), "9");
+    }
+
+    @Test
+    public void updateBoardSpaceWithNewValue() {
+        board = new Board();
+        board.updateBoard(2, "X");
+
+        Assert.assertEquals(board.getSpace(2), "X");
     }
 
 }
