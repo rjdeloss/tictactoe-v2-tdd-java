aad94db63fa6cbcd750d2bf39f45dea84113b09b
Rafiluis De Los Santos <rjdeloss@gmail.com>
2022-05-04 14:41:53 -0400

    create test for createInstanceOfHumanPlayer

--- app/src/test/java/tttTDD/Java/PlayerTest.java
+++ app/src/test/java/tttTDD/Java/PlayerTest.java
@@ -1,12 +1,20 @@
 package tttTDD.Java;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class PlayerTest{
+    Player human;
+
+    @Before
+    public void setup() {
+
+    }
 
     @Test
-    public void passingTest() {
-        Assert.assertEquals(6, 6);
+    public void createInstanceOfHumanPlayer() {
+        human = new Player("X");
+        Assert.assertEquals(human.getMarker(), "X");
     }
 }
