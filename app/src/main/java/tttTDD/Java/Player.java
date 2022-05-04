ae765e19e96406e0a34a6aab6666d3633df7631f
Rafiluis De Los Santos <rjdeloss@gmail.com>
2022-05-04 14:53:27 -0400

    write tests for the Player class TDD style

--- app/src/main/java/tttTDD/Java/Player.java
+++ app/src/main/java/tttTDD/Java/Player.java
@@ -1,4 +1,19 @@
 package tttTDD.Java;
 
 public class Player {
+    private String marker;
+    private boolean ai;
+
+    Player(String marker, boolean isComputer) {
+        this.marker = marker;
+        this.ai = isComputer;
+    }
+
+    public String getMarker() {
+        return this.marker;
+    }
+
+    public boolean isComputer() {
+        return this.ai;
+    }
 }
--- app/src/test/java/tttTDD/Java/PlayerTest.java
+++ app/src/test/java/tttTDD/Java/PlayerTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 public class PlayerTest{
     Player human;
+    Player computer;
 
     @Before
     public void setup() {
@@ -14,7 +15,25 @@ public class PlayerTest{
 
     @Test
     public void createInstanceOfHumanPlayer() {
-        human = new Player("X");
+        human = new Player("X", false);
         Assert.assertEquals(human.getMarker(), "X");
+    }
+
+    @Test
+    public void verifyTheCorrectMarkerHasBeenAssigned() {
+        human = new Player("S", false);
+        Assert.assertNotEquals(human.getMarker(), "X");
+    }
+
+    @Test
+    public void verifyThePlayerBeingHuman() {
+        human = new Player("O", false);
+        Assert.assertEquals(human.isComputer(), false);
+    }
+
+    @Test
+    public void verifyThePlayerBeingAComputer() {
+        computer = new Player("X", true);
+        Assert.assertEquals(computer.isComputer(), true);
     }
 }
