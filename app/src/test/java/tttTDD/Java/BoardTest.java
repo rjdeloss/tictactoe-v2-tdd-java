fe99141d58fee19d23741cc1bebc90d67a7869d2
Rafiluis De Los Santos <rjdeloss@gmail.com>
2022-05-04 15:02:21 -0400

    create first test for the Board class

--- app/src/test/java/tttTDD/Java/BoardTest.java
+++ app/src/test/java/tttTDD/Java/BoardTest.java
@@ -1,4 +1,16 @@
 package tttTDD.Java;
 
+
+import org.junit.Assert;
+import org.junit.Test;
+
 public class BoardTest {
+    Board board;
+
+    @Test
+    public void getBoardInstance() {
+        board = new Board();
+        Assert.assertEquals(board.getBoard().length, 9);
+    }
+
 }
