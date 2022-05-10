4018f775d32ab0dc673369789575ab420eb14bef
Rafiluis De Los Santos <rjdeloss@gmail.com>
2022-05-10 10:12:36 -0400

    separate Player class into Human and Computer classes and change Player into an interface

---
+++ app/src/main/java/tttTDD/Java/Computer.java
@@ -0,0 +1,21 @@
+package tttTDD.Java;
+
+import tttTDD.Java.Interfaces.Player;
+
+public class Computer implements Player {
+    private final String marker;
+
+    public Computer(String marker) {
+        this.marker = marker;
+    }
+
+    @Override
+    public String getMarker() {
+        return this.marker;
+    }
+
+    @Override
+    public boolean isComputer() {
+        return true;
+    }
+}
--- app/src/test/java/tttTDD/Java/PlayerTest.java
+++ app/src/test/java/tttTDD/Java/PlayerTest.java
@@ -5,8 +5,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class PlayerTest{
-    Player human;
-    Player computer;
+    Human human;
+    Computer computer;
 
     @Before
     public void setup() {
@@ -15,26 +15,26 @@ public class PlayerTest{
 
     @Test
     public void createInstanceOfAPlayer() {
-        human = new Player("X", false);
+        human = new Human("X");
         Assert.assertEquals(human.getMarker(), "X");
     }
 
     @Test
     public void verifyTheCorrectMarkerHasBeenAssigned() {
-        human = new Player("S", false);
+        human = new Human("S");
         Assert.assertNotEquals(human.getMarker(), "X");
         Assert.assertEquals(human.getMarker(), "S");
     }
 
     @Test
     public void verifyThePlayerBeingHuman() {
-        human = new Player("O", false);
+        human = new Human("O");
         Assert.assertEquals(human.isComputer(), false);
     }
 
     @Test
     public void verifyThePlayerBeingAComputer() {
-        computer = new Player("X", true);
+        computer = new Computer("X");
         Assert.assertEquals(computer.isComputer(), true);
     }
 }
