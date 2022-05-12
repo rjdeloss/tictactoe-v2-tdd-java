c8df238caa8f1647bb8cf04275fa543321179726
Rafiluis De Los Santos <rjdeloss@gmail.com>
2022-05-12 09:43:54 -0400

    pairning session with Bill

--- app/src/main/java/tttTDD/Java/Game.java
+++ app/src/main/java/tttTDD/Java/Game.java
@@ -1,8 +1,26 @@
 package tttTDD.Java;
 
+import tttTDD.Java.Interfaces.Player;
+
 public class Game {
+    Board board;
+    Player player1;
+    Player currentPlayer;
+//    GameConfiguration gameConfig = new GameConfiguration(3, true);
 
-    public Game() {
+    public Game(GameConfiguration gameConfig) {
+        board = new Board(gameConfig.getBoardSizeConfiguration());
+        intializePlayers(gameConfig.isComputerPlaying());
+        currentPlayer = player1;
+    }
 
+    public boolean performMove(int playerInput) {
+        board.updateBoard(playerInput, currentPlayer.getMarker());
+
+        return true;
+    }
+
+    private void intializePlayers(boolean areWePlayingComputer) {
+        player1 = new Human("X");
     }
 }
--- app/src/test/java/tttTDD/Java/GameTest.java
+++ app/src/test/java/tttTDD/Java/GameTest.java
@@ -5,19 +5,28 @@ import org.junit.Before;
 import org.junit.Test;
 
 import javax.swing.*;
+import java.lang.module.Configuration;
 
 public class GameTest {
     Game game;
+    GameConfiguration gameConfig;
 
-//    @Test
-//    public void PlayerShouldUpdateBoardWithinTheGame3x3() {
-//        // Arrange
-//        Game classUnderTest = new Game();
-//
-//        // Act
-//        String[] state = classUnderTest.performTurn(3);
-//
-//        // Assert
-//        Assert.assertEquals(board[3], "X");
-//    }
+    @Before
+    public void setup() {
+        gameConfig = new GameConfiguration(3, true);
+    }
+
+    @Test
+    public void PlayerShouldUpdateBoardWithinTheGame3x3() {
+        // Arrange
+        int playerInput = 3;
+        game = new Game(gameConfig);
+
+
+        // Act
+        game.performMove(playerInput);
+
+        // Assert
+        Assert.assertEquals(game.board.getSpace(playerInput), game.currentPlayer.getMarker());
+    }
 }
